86ebcc0326267ba9f95966618e28b600dce6fa59
Umbr0o <slimshady1904@mail.ru>
2022-09-21 16:42:34 +0300

    Creating the necessary fields to add a new employee

--- trainingtask/Controllers/EmployeeEditViewController.swift
+++ trainingtask/Controllers/EmployeeEditViewController.swift
@@ -8,5 +8,60 @@
 import UIKit
 
 class EmployeeEditViewController: UIViewController {
+    //MARK: - Private property
     
+    private let surnameTextField = MyTextField()
+    private let nameTextField = MyTextField()
+    private let patronymicTextField = MyTextField()
+    private let positionTextField = MyTextField()
+    
+    private var saveButton = UIBarButtonItem()
+    private var cancelButton = UIBarButtonItem()
+    
+    //MARK: - VC lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.backgroundColor = .systemGray6
+        view.backgroundColor = .systemGray6
+        
+        title = "Добавление сотрудника"
+    }
+    
+    //MARK: - Setup function
+    
+    private func setup() {
+        view.addSubview(surnameTextField)
+        view.addSubview(nameTextField)
+        view.addSubview(patronymicTextField)
+        view.addSubview(positionTextField)
+        
+        NSLayoutConstraint.activate([
+            surnameTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            surnameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            surnameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            
+            nameTextField.topAnchor.constraint(equalTo: surnameTextField.bottomAnchor, constant: 50),
+            nameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            nameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            
+            patronymicTextField.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 50),
+            patronymicTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            patronymicTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            
+            positionTextField.topAnchor.constraint(equalTo: patronymicTextField.bottomAnchor, constant: 50),
+            positionTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            positionTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+        ])
+        
+        surnameTextField.placeholder = "Фамилия"
+        nameTextField.placeholder = "Имя"
+        patronymicTextField.placeholder = "Отчество"
+        positionTextField.placeholder = "Должность"
+    }
 }
---
+++ trainingtask/Custom Views/MyTextField.swift
@@ -0,0 +1,24 @@
+//
+//  MyTextField.swift
+//  trainingtask
+//
+//  Created by Артем Томило on 21.09.22.
+//
+
+import UIKit
+
+class MyTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 5
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.lightGray.cgColor
+        borderStyle = .roundedRect
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+
